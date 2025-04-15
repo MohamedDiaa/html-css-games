86a669c03106b465715ccf7534959ac5112e98a7
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2025-04-15 16:03:52 +0200

    fixing mine sweeper logic

---
+++ exp.js
@@ -0,0 +1,42 @@
+// let map = new Array(9).fill(null).map(() => new Array(9).fill(""));
+// const mines = [
+//   [1, 7],
+//   [3, 2],
+//   [4, 7],
+//   [6, 0],
+//   [6, 1],
+//   [6, 2],
+//   [6, 3],
+//   [7, 4],
+//   [7, 5],
+//   [8, 3],
+//   [8, 5],
+// ];
+
+// for (let i = 0; i < 9; i++) {
+//   for (let j = 0; j < 9; j++) {
+
+//     for (let k = 0; k < mines.length; k++) {
+//       let [a, b] = mines[k];
+//       if ((a == i) && (b == j)) {
+//         map[i][j] = "Y";
+//       } 
+//     }
+//   }
+// }
+
+
+
+// for (let i = 0; i < 9; i++) {
+//     let row = "";
+//     //  console.log("row: ", i);
+//       for (let j = 0; j < 9; j++) {
+//       //  
+//         row += map[i][j] + " " ;
+//       }
+
+//       console.log(row);
+//       row = "";
+//     }
+
+
